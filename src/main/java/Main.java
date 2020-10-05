890c00e57734634f616fe23c2befb1d608258e65
Terrile John <terrilejohn24@gmail.com>
2020-10-04 20:00:34 -0700

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+/**
+ * Take a Spotify playlist and transfer it over into a Youtube
+ * playlist, possibly on a loop.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Transfer_Methods test=new Transfer_Methods();
+        test.get_playlist(System.getProperty("playlist_id"));
+
+    }
+}
---
+++ src/main/java/Transfer_Methods.java
@@ -0,0 +1,52 @@
+/**
+ * Take a Spotify playlist and transfer it over into a Youtube
+ * playlist, possibly on a loop.
+ */
+public class Transfer_Methods {
+    public Transfer_Methods(){}
+
+    /**
+     * Log into spotify.
+     */
+    public void get_Spotify_client() {
+    }
+
+    /**
+     * Grab desired playlist to transfer.
+     *
+     * @param playlist_id - name of the playlist.
+     * @return String - Song names unparsed.
+     */
+    public String get_playlist(String playlist_id) {
+    }
+
+    /**
+     * Log into Youtube.
+     */
+    public void get_Youtube_client() {
+
+    }
+
+    /**
+     * Create a youtube playlist.
+     */
+    public void create_playlist() {
+
+    }
+
+    /**
+     * Search Youtube for song from spotify playlist.
+     */
+    public void search_song() {
+
+    }
+
+    /**
+     * Add song into youtube playlist.git
+     */
+    public void add_song() {
+
+    }
+
+
+}
