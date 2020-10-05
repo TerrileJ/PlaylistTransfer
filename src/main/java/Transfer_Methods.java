77b965c44f5a6ebe175fa982707468cfbdadb078
Terrile John <terrilejohn24@gmail.com>
2020-10-04 20:06:33 -0700

    Able to get Spotify track names

--- src/main/java/Transfer_Methods.java
+++ src/main/java/Transfer_Methods.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Take a Spotify playlist and transfer it over into a Youtube
  * playlist, possibly on a loop.
@@ -17,7 +23,39 @@ public class Transfer_Methods {
      * @param playlist_id - name of the playlist.
      * @return String - Song names unparsed.
      */
-    public String get_playlist(String playlist_id) {
+    public String get_playlist(String playlist_id) throws IOException {
+        // System Property for user's Oauth token
+        String Oauth_value = System.getProperty("Oauth_token");
+        //System.out.println(playlist_id);
+        //System.out.println(Oauth_value);
+
+        // java http request to Spotify Web api
+        URL url = new URL("https://api.spotify.com/v1/playlists/" + playlist_id + "/tracks?fields=items(track(name))");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+
+        // setting headers
+        con.setRequestProperty("Accept", "application-json");
+        con.setRequestProperty("Authorization", " Bearer " + Oauth_value);
+        con.setRequestProperty("Content-Type", "application-json");
+
+        int status = con.getResponseCode();
+        String statusM = con.getResponseMessage();
+        System.out.print(status);
+        System.out.println(statusM);
+
+        // read in input
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+
+        // close the connection
+        con.disconnect();
+        return content.toString();
     }
 
     /**
